42b58695b7d5d3414008bc8a30d78a8e10a7f8d8
chlavieri <chlavieri@github.com>
2021-03-27 23:48:55 -0300

    Arrumando a base ate a plotagem

--- Brasil-municipios/pop_municipios.py
+++ Brasil-municipios/pop_municipios.py
@@ -6,15 +6,41 @@ Created on Thu Mar 25 22:03:16 2021
 @author: chlav
 """
 
+## Chama os pacotes
+
 import geobr
 import os
 import geopandas as gpd
 import geoplot
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+import re
 
-br = geobr.read_country(year = 2019)
 
-pop2020 = pd.read_excel("Brasil-municipios/Data/estimativa_dou_2020.xls",
-                        dtype = {"COD. MUNIC" : str})
+## Le as bases de shapefile dos municipios e de populacao
+br = geobr.read_municipality(code_muni="all", year=2019)
+
+pop = pd.read_excel("Brasil-municipios/Data/estimativa_dou_2020.xls",
+                        dtype = {"COD. MUNIC" : str,
+                                 "POPULAÇÃO ESTIMADA" : str})
+
+## Arruma a base de pop e junta com a dos municipios
+pop2 = pop.dropna()
+
+pop2["POPULAÇÃO ESTIMADA"] = [int(re.sub("\(.*\)", "",
+                            i)) for i in list(pop2["POPULAÇÃO ESTIMADA"])]
+
+pop3 = pop2.rename(columns = {"COD. UF" : "code_state",
+                    "NOME DO MUNICÍPIO" : "name_muni", "UF" : "abbrev_state"})
+
+
+brpop = pd.merge(br, pop3, on = "name_muni", how = "inner")
+
+## Plotagem
+
+mpl.style.use("seaborn")
+fig, ax = plt.subplots(1, 1)
+
+
+brpop.plot(column = "POPULAÇÃO ESTIMADA", cmap = "Dark2")
